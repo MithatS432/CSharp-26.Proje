178c1bc992f23ee342f5b53a92d2c96068bd1279
Mithat <mithatsirmali@gmail.com>
2025-05-24 15:47:18 +0300

    Add files via upload

---
+++ Çok Biçimlilik (Polymorphism)2/Çok Biçimlilik (Polymorphism)2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Çok_Biçimlilik__Polymorphism_2
+{
+    internal class Program
+    {
+        class Sekil
+        {
+            public virtual void Ciz()
+            {
+                Console.WriteLine("Bir şekil çiziliyor.");
+            }
+        }
+
+        class Daire : Sekil
+        {
+            public override void Ciz()
+            {
+                Console.WriteLine("Bir daire çiziliyor.");
+            }
+        }
+
+        class Kare : Sekil
+        {
+            public override void Ciz()
+            {
+                Console.WriteLine("Bir kare çiziliyor.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Sekil s1 = new Daire(); // Polimorfizm!
+            Sekil s2 = new Kare();  // Polimorfizm!
+
+            s1.Ciz(); // daire çiziliyor
+            s2.Ciz(); // kare çiziliyor
+        }
+    }
+}
